809970224188d9d916380eea190007d8e7283278
Muhammad-Ali A'rabi <angellandros@yahoo.com>
2017-08-28 07:03:52 +0200

    Data Controller

--- whiskey-tango/app/controllers/DataController.scala
+++ whiskey-tango/app/controllers/DataController.scala
@@ -5,22 +5,35 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
 @Singleton
 class DataController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+  val conf = new SparkConf()
+    .setJars(Array("target/scala-2.11/whiskey-tango_2.11-1.0-SNAPSHOT.jar"))
+    .setAppName("City Aggregator")
+    .setMaster("spark://Zuse5:7077")
+  val sc = new SparkContext(conf)
+
   def cities(from: String, until: String, city: String) = Action { implicit request: Request[AnyContent] =>
+    val format = new java.text.SimpleDateFormat("yyyy-MM-dd")
     val m = Map("from" -> from, "until" -> until, "city" -> city)
-    Ok(Json.toJson(m))
+    val textFile = sc.textFile("../input.csv")
+    val data = textFile
+      .map(_.split(";"))
+      .map(s => (s(0), (s(3), s(9))))
+      .reduceByKey((i, j) => i)
+      .map(_._2)
+      .filter(p => format.parse(p._1) after format.parse(m("from")))
+      .filter(p => format.parse(p._1) before format.parse(m("until")))
+      .filter(_._2 matches m("city"))
+      .map(_ -> 1)
+      .reduceByKey(_+_)
+      .collect()
+    Ok(Json.toJson(data))
   }
+
 }
--- whiskey-tango/build.sbt
+++ whiskey-tango/build.sbt
@@ -5,10 +5,21 @@ version := "1.0-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
-scalaVersion := "2.12.2"
+scalaVersion := "2.11.8"
+
+
+dependencyOverrides += "com.fasterxml.jackson.core" % "jackson-core" % "2.8.9"
+dependencyOverrides += "com.fasterxml.jackson.core" % "jackson-databind" % "2.8.9"
+dependencyOverrides += "com.fasterxml.jackson.module" % "jackson-module-scala_2.11" % "2.8.9"
+
+dependencyOverrides += "org.apache.hadoop" % "hadoop-mapreduce-client-core" % "2.7.2"
+dependencyOverrides += "org.apache.hadoop" % "hadoop-common" % "2.7.2"
+dependencyOverrides += "commons-io" % "commons-io" % "2.4"
 
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.0" % Test
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
 
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "me.angellandros.controllers._"
